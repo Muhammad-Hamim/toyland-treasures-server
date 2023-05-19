31821be4b44785a954e42f6c684866481c1af8bf
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-19 11:45:25 +0600

    added mongodb successfully also implement the dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Toyland_Treasures_Admin;
-// 2m87HGQa8f1QS0gO
-
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.xmeadqe.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmeadqe.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,19 +24,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-    const galleryCollection = client.db("ToylandTreasures").collection("gallery");
+    const galleryCollection = client
+      .db("ToylandTreasures")
+      .collection("gallery");
 
     app.get("/gallery", async (req, res) => {
       const cursor = galleryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-
-
-
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
