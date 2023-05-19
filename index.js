cf88ba8c85c3e353d39cc5fae85cd2d486a114c1
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-19 10:45:22 +0600

    created server and all setup done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Toyland Treasures!!");
+});
+
+app.listen(port, () => {
+  console.log(`toyland is running on port ${port}`);
+});
